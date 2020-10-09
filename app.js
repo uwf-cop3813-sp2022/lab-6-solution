6d3db3de198818673d98e86b2490ace6d2b05907
efridge <efridge@uwf.edu>
2020-10-09 08:21:33 -0500

    Finishing up the lab.

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 let app = express();
+
+// Set up the body parser
+app.use( bodyParser.urlencoded( {extended: true}) );
+
+// Set up cookie parser and sessions
+const COOKIE_SECRET = 'keyboard cat';								// My secret to secure cookies
+app.use( require('cookie-parser')(COOKIE_SECRET) ); // Parse incoming cookies
+
+// Set up session for the user, based on cookies
+app.use(session({
+  secret: COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // set up handlebars view engine
 let handlebars = require('express-handlebars')
@@ -10,11 +27,36 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(__dirname + '/public'));
-
+app.use( express.static(__dirname + '/public') );
 
 app.get('/', function(req, res) {
-	res.render('home');
+
+	// If they are logged in (have a user object set)
+	if(req.session.user) {
+
+		// Render the home route and use user data
+		res.render('home', {
+			user: req.session.user
+		});
+
+	// Otherwise ask them to log in again
+	} else {
+		res.render('login', {
+			error: "You need to log in!"
+		});
+	}
+
+});
+
+// Display the login prompt
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+
+// Handle the login action
+app.post('/login', function(req, res) {
+	req.session.user = req.body.username;	// Set the username
+	res.redirect('/');
 });
 
 // 404 catch-all handler (middleware)
